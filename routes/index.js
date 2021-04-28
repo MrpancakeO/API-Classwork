b1c103614ee6d954c8bbb0c061b35945941b5270
Brandon Chansamone <braakson@yahoo.com>
2021-04-27 21:32:57 -0600

    finished first video code

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ courseRoutes = require("./courseRoutes"),
 homeRoutes = require("./homeRoutes"),
 errorRoutes = require("./errorRoutes");
 
-router.use("/user", userRoutes);
+router.use("/users", userRoutes);
 router.use("/subscribers", subscriberRoutes);
 router.use("/courses", courseRoutes);
 router.use("/", homeRoutes);
-router.use("/error", errorRoutes);
+router.use("/", errorRoutes);
 
 module.exports = router;
